b3b666165626511839bcdb5dd13933519d68a0e2
evandro-webdev <evandromateus06@gmail.com>
2025-07-29 17:25:53 -0300

    wip

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
@@ -11,5 +13,53 @@ class ProfileController extends Controller
     $user = Auth::user();
     
     return view('auth.profile', ['user' => $user]);
+  }
+
+  public function update(Request $request)
+  {
+    $attributes = $request->validate([
+      'name' => ['required'],
+      'email' => ['required', 'email', 'unique:users,email'],
+      'bio' => ['nullable', 'string', 'max:500'],
+
+      'social_networks' => ['nullable', 'array'],
+      'social_networks.*.provider' => ['required', 'string', 'in:twitter, instagram, facebook, linkedin, github, website, youtube'],
+      'social_networks.*.url' => ['required', 'url']
+    ]);
+
+    DB::transaction(function () use ($request, $attributes){
+      $user = Auth::user();
+
+      $user->update([
+        'name' => $attributes['name'],
+        'email' => $attributes['email'],
+        'bio' => $attributes['bio'] ?? null,
+      ]);
+
+      if($request->has('social_networks')){
+        $this->syncSocialProfiles($user, $attributes['social_networks']);
+      }
+    });
+
+    return response()->json(['success' => 'Perfil atualizado']);
+  }
+
+  protected function syncSocialProfiles(User $user, array $socialNetworks){
+    $user->socialProfiles()->delete();
+
+    foreach($socialNetworks as $network){
+      $user->socialProfiles()->create([
+        'provider' => $network['provider'],
+        'url' => $this->normalizeUrl($network['url'], $network['provider']),
+      ]);
+    }
+  }
+
+  protected function normalizeUrl($url, $provider){
+    if(!preg_match("~^(?:f|ht)tps?://~i", $url)){
+      return "https://" . $url;
+    }
+
+    return $url;
   }
 }
--- resources/views/components/profile/tabs/profile.blade.php
+++ resources/views/components/profile/tabs/profile.blade.php
@@ -1,25 +1,29 @@
 <div x-show="tab === 'profile'" class="space-y-6">
-  <x-ui.panel>
-    <x-section-heading>Informações pessoais</x-section-heading>
+  <form action="" class="space-y-6">
+    <x-ui.panel>
+      <x-section-heading>Informações pessoais</x-section-heading>
+        <div class="space-y-4">
+          <x-ui.forms.input name="name" value="{{ $user->name }}" label="Nome" placeholder="Nome"/>
+          <x-ui.forms.input name="email" value="{{ $user->email }}" type="email" label="Email" placeholder="Email"/>
+          <x-ui.forms.input name="bio" label="Bio" placeholder="Bio" as="textarea"/>
+          <x-ui.forms.input name="location" label="Cidade" placeholder="Cidade"/>
+        </div>
+    </x-ui.panel>
 
-    <form action="" class="space-y-4">
-      <x-ui.forms.input name="name" value="{{ $user->name }}" label="Nome" placeholder="Nome"/>
-      <x-ui.forms.input name="email" value="{{ $user->email }}" type="email" label="Email" placeholder="Email"/>
-      <x-ui.forms.input name="bio" label="Bio" placeholder="Bio" as="textarea"/>
-      <x-ui.forms.input name="location" label="Cidade" placeholder="Cidade"/>
-    </form>
-  </x-ui.panel>
+    <x-ui.panel>
+      <x-section-heading>Redes sociais</x-section-heading>
+      <div class="space-y-4">
+        <input type="hidden" name="social_networks[0][provider]" value="twitter">
+        <x-ui.forms.input type="url" name="social_networks[0][url]" label="Twitter" placeholder="https://twitter.com/..." />
+        
+        <input type="hidden" name="social_networks[1][provider]" value="github">
+        <x-ui.forms.input type="url" name="social_networks[1][url]" label="Github" placeholder="https://github.com/..." />
 
-  <x-ui.panel>
-    <x-section-heading>Redes sociais</x-section-heading>
-
-    <form action="" class="space-y-4">
-      <x-ui.forms.input name="x" label="X" placeholder="X"/>
-      <x-ui.forms.input name="instagram" label="Instagram" placeholder="Bio" as="Instagram"/>
-      <x-ui.forms.input name="linkedin" label="Linkedin" placeholder="Linkedin"/>
-      <x-ui.forms.input name="github" label="Github" placeholder="Github"/>
-    </form>
-  </x-ui.panel>
+        <input type="hidden" name="social_networks[1][provider]" value="instagram">
+        <x-ui.forms.input type="url" name="social_networks[1][url]" label="Instagram" placeholder="https://instagram.com/..." />
+      </div>
+    </x-ui.panel>
+  </form>
 
   <x-ui.panel>
     <x-section-heading>Segurança</x-section-heading>
