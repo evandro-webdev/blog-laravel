111b6eeefa2fb4c0d40efe6a5efa89a393cba91a
evandro-webdev <evandromateus06@gmail.com>
2025-08-01 17:34:40 -0300

    feat: create ProfileRequest

---
+++ app/Http/Requests/ProfileRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class ProfileRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+      return [
+        'name' => ['require"d', 'string', 'max:255'],
+        'email' => ['required', 'email', Rule::unique('users')->ignore(Auth::id())],
+        'bio' => ['nullable', 'string', 'max:500'],
+        'city' => ['nullable', 'string', 'max:85'],
+        
+        'twitter' => ['nullable', 'url', 'max:255'],
+        'github' => ['nullable', 'url', 'max:255'],
+        'instagram' => ['nullable', 'url', 'max:255'],
+        'linkedin' => ['nullable', 'url', 'max:255'],
+        'youtube' => ['nullable', 'url', 'max:255'],
+      ];
+    }
+}
