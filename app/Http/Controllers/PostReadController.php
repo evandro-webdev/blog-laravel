df14a39e91306c6e3cbc5aa55e04b2de18b6f6d7
evandro-webdev <evandromateus06@gmail.com>
2025-08-28 16:21:58 -0300

    feat: create PostReadController

---
+++ app/Http/Controllers/PostReadController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class PostReadController extends Controller
+{
+  public function store(Post $post)
+  {
+    $post->markAsRead(Auth::user());
+
+    return response()->json([
+      'message' => 'Post marcado como lido',
+      'read' => true
+    ]);
+  }
+
+  public function destroy(Post $post)
+  {
+    $post->markAsUnread(Auth::user());
+
+    return response()->json([
+      'message' => 'Post marcado como não lido',
+      'read' => false
+    ]);
+  }
+}
