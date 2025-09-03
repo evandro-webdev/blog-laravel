61f532804ff921c03ec328a72ee353233103b4e0
evandro-webdev <evandromateus06@gmail.com>
2025-09-03 17:16:58 -0300

    chore: register CommentAdded event

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,20 +2,25 @@
 
 namespace App\Providers;
 
+use App\Events\CommentAdded;
 use App\Events\PostPublished;
 use App\Events\UserFollowed;
+use App\Listeners\CreateCommentAddedNotification;
 use App\Listeners\CreateFollowNotification;
 use App\Listeners\CreatePostPublishedNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
 {
-    protected $listen = [
-      UserFollowed::class => [
-        CreateFollowNotification::class,
-      ],
-      PostPublished::class => [
-        CreatePostPublishedNotification::class
-      ],
-    ];
+  protected $listen = [
+    UserFollowed::class => [
+      CreateFollowNotification::class,
+    ],
+    PostPublished::class => [
+      CreatePostPublishedNotification::class
+    ],
+    CommentAdded::class => [
+      CreateCommentAddedNotification::class
+    ],
+  ];
 }
