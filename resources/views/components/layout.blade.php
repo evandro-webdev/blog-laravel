5a94d64d49bd0a754664d55e0c5bc3d1b8d0c3c3
evandro-webdev <evandromateus06@gmail.com>
2025-09-05 16:18:33 -0300

    chore: import alpine collapse plugin

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   @vite(['resources/js/app.js'])
   @stack('scripts')
+  <script defer src="https://cdn.jsdelivr.net/npm/@alpinejs/collapse@3.x.x/dist/cdn.min.js"></script>
   <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
   <title>Blog Tecnologia</title>
 </head>
