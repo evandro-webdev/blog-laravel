e3799b1dd640d33638a9ea71425a45971dddd9f6
evandro-webdev <evandromateus06@gmail.com>
2025-09-17 16:49:44 -0300

    update: minor visual changes

--- resources/views/components/nav/notifications/notification-item.blade.php
+++ resources/views/components/nav/notifications/notification-item.blade.php
@@ -1,10 +1,10 @@
 <div class="p-3 flex gap-2 transition-colors hover:bg-gray-50 dark:hover:bg-gray-900 border-l-4 
-  {{ is_null($notification->read_at) ? 'border-blue-500 bg-blue-50' : 'border-transparent' }}"
+  {{ is_null($notification->read_at) ? 'border-blue-500 bg-blue-50 dark:bg-gray-700' : 'border-transparent' }}"
 >
   <x-profile.avatar :user="$notification->actor" />
 
   <div class="flex flex-col gap-1">
-    <a href="#" class="text-sm font-medium text-gray-800 dark:text-white">
+    <a href="#" class="text-sm font-medium line-clamp-2 text-gray-800 dark:text-white">
       {{ $notification->data['message'] }}
     </a>
     <time class="text-xs text-gray-600 dark:text-gray-100" datetime="{{ $notification->created_at }}">
