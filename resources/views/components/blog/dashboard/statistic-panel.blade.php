a6c5642efdb52377129f671a653310876f70bdb9
evandro-webdev <evandromateus06@gmail.com>
2025-09-22 17:34:42 -0300

    feat: create statistic-panel component

---
+++ resources/views/components/blog/dashboard/statistic-panel.blade.php
@@ -0,0 +1,16 @@
+@props([
+  'icon',
+  'label',
+  'statistic'
+])
+
+<x-ui.panel class="space-y-4 hover:bg-gray-50 dark:hover:bg-gray-950">
+  <div class="flex justify-between">
+    <span class="font-medium text-gray-900 dark:text-white">{{ $label }}</span>
+    <x-dynamic-component :component="'ui.icons.' . $icon" class="text-blue-600 dark:text-blue-500"/>
+  </div>
+  <div>
+    <span class="text-3xl font-bold text-gray-800 dark:text-white">{{ $statistic['total'] }}</span>
+    <p class="text-sm text-gray-700 dark:text-gray-200">+{{ $statistic['last_30_days'] }} no ultimo mês</p>
+  </div>
+</x-ui.panel>
--- resources/views/components/blog/dashboard/tabs/tab-overview.blade.php
+++ resources/views/components/blog/dashboard/tabs/tab-overview.blade.php
@@ -1,48 +1,9 @@
 <div x-show="tab === 'overview'" class="space-y-2">
   <div class="grid gap-2 sm:grid-cols-2 lg:grid-cols-4">
-    <x-ui.panel class="space-y-4 hover:bg-gray-50 dark:hover:bg-gray-950">
-      <div class="flex justify-between">
-        <span class="font-medium text-gray-900 dark:text-white">Total Posts</span>
-        <x-ui.icons.doc class="text-blue-600 dark:text-blue-500"/>
-      </div>
-      <div>
-        <span class="text-3xl font-bold text-gray-800 dark:text-white">{{ $statistics['totalPosts'] }}</span>
-        <p class="text-sm text-gray-700 dark:text-gray-200">+{{ $statistics['publishedLast30Days'] }} no ultimo mês</p>
-      </div>
-    </x-ui.panel>
-    
-    <x-ui.panel class="space-y-4 hover:bg-gray-50 dark:hover:bg-gray-950">
-      <div class="flex justify-between">
-        <span class="font-medium text-gray-900 dark:text-white">Total Views</span>
-        <x-ui.icons.eye class="text-blue-600 dark:text-blue-500"/>
-      </div>
-      <div>
-        <span class="text-3xl font-bold text-gray-800 dark:text-white">{{ $statistics['totalViews'] }}</span>
-        <p class="text-sm text-gray-700 dark:text-gray-200">+18% no ultimo mês</p>
-      </div>
-    </x-ui.panel>
-
-    <x-ui.panel class="space-y-4 hover:bg-gray-50 dark:hover:bg-gray-950">
-      <div class="flex justify-between">
-        <span class="font-medium text-gray-900 dark:text-white">Inscrições</span>
-        <x-ui.icons.users class="text-blue-600 dark:text-blue-500"/>
-      </div>
-      <div>
-        <span class="text-3xl font-bold text-gray-800 dark:text-white">573</span>
-        <p class="text-sm text-gray-700 dark:text-gray-200">+38 nesse mês</p>
-      </div>
-    </x-ui.panel>
-
-    <x-ui.panel class="space-y-4 hover:bg-gray-50 dark:hover:bg-gray-950">
-      <div class="flex justify-between">
-        <span class="font-medium text-gray-900 dark:text-white">Engajamento</span>
-        <x-ui.icons.arrow-trending class="text-blue-600 dark:text-blue-500"/>
-      </div>
-      <div>
-        <span class="text-3xl font-bold text-gray-800 dark:text-white">12.4K</span>
-        <p class="text-sm text-gray-700 dark:text-gray-200">+18% no ultimo mês</p>
-      </div>
-    </x-ui.panel>
+    <x-blog.dashboard.statistic-panel label="Publicações" icon="doc" :statistic="$statistics['posts']"/>
+    <x-blog.dashboard.statistic-panel label="Visualizações" icon="eye" :statistic="$statistics['views']"/>
+    <x-blog.dashboard.statistic-panel label="Engajamento" icon="chart-bar" :statistic="$statistics['comments']"/>
+    <x-blog.dashboard.statistic-panel label="Seguidores" icon="users" :statistic="$statistics['followers']"/>
   </div>
 
   <x-ui.panel>
@@ -63,7 +24,7 @@
     </div>
   </x-ui.panel>
 
-  <x-ui.panel class="w-1/2">
+  {{-- <x-ui.panel class="w-1/2">
     @php
       $maxCount = $popularCategories->max('posts_count');
     @endphp
@@ -87,5 +48,5 @@
         </li>
       @endforeach
     </ul>
-  </x-ui.panel>
+  </x-ui.panel> --}}
 </div>
