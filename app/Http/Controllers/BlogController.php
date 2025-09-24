c49663a41cfb48de241c60876b45b9538b8e8d6d
evandro-webdev <evandromateus06@gmail.com>
2025-09-24 17:34:38 -0300

    update: refactor BlogController

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -20,38 +20,26 @@ class BlogController extends Controller
   {
     $user = Auth::user();
     $tab = $request->query('tab', $user ? 'personal-feed' : 'trending-feed');
-    $sort = $this->resolveSort($request, $tab);
+    $sort = $request->query('sort');
 
-    $posts = $this->getFilteredPosts($tab, $sort, $user);
+    $posts = $this->postService->getFeed($user, $sort, $tab);
+    $trendingPostsThisMonth = $this->postService->getTrendingPostsInPeriod(30);
 
     $usersToFollow = $this->getUsersToFollow($user);
-      
+
     return view('home.index', [
       'user' => $user,
       'posts' => $posts,
+      'trendingPostsThisMonth' => $trendingPostsThisMonth,
       'sort' => $sort,
       'tab' => $tab,
       'usersToFollow' => $usersToFollow,
     ]);
   }
 
-  private function getFilteredPosts(string $tab, string $sort, ?User $user){
-    if($tab === 'personal-feed' && $user){
-      return $this->postService->getFeed($user, $sort);
-    } else {
-      return $this->postService->getFeed(null, $sort);
-    }
-  }
-
   private function getUsersToFollow(?User $user, int $limit = 5){
     return $user 
-      ? $user->notFollowing()->limit(5)->get() 
-      : User::limit(5)->get();
-  }
-
-  private function resolveSort(Request $request, string $tab): string
-  {
-    $default = $tab === 'trending-feed' ? 'popular' : 'recent';
-    return $request->query('sort', $default);
+      ? $user->notFollowing()->limit($limit)->get() 
+      : User::limit($limit)->get();
   }
 }
