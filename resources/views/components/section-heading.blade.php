a18b2482e4cbd08d7573f1df05848321a6f13019
evandro-webdev <evandromateus06@gmail.com>
2025-09-24 17:35:45 -0300

    update: refactor section-heading component

--- resources/views/components/section-heading.blade.php
+++ resources/views/components/section-heading.blade.php
@@ -1,8 +1,36 @@
-@props(['link' => false])
+@props([
+  'title',
+  'desc' => null,
+  'link' => false,
+  'size' => 'md',
+])
 
-<div class="mb-8 flex items-center justify-between">
-  <h2 class="text-3xl font-bold text-gray-900 dark:text-white">{{ $slot }}</h2>
+@php
+  $sizes = [
+    'sm' => ['title' => 'text-lg',  'desc' => 'text-xs', 'link' => 'text-xs'],
+    'md' => ['title' => 'text-2xl', 'desc' => 'text-sm', 'link' => 'text-sm'],
+    'lg' => ['title' => 'text-4xl', 'desc' => 'text-base', 'link' => 'text-base'],
+  ];
+
+  $class = $link ? 'flex items-center justify-between' : '';
+@endphp
+
+<div {{ $attributes->merge(['class' => $class]) }}>
+  <div class="space-y-1">
+    <h2 class="{{ $sizes[$size]['title'] }} font-bold text-gray-900 dark:text-white">
+      {{ $title }}
+    </h2>
+
+    @if ($desc)
+      <p class="{{ $sizes[$size]['desc'] }} text-gray-700 dark:text-gray-100">
+        {{ $desc }}
+      </p>
+    @endif
+  </div>
+  
   @if ($link)
-    <a href="{{ $link }}" class="text-sm font-medium text-gray-700">Ver todos →</a>
+    <a href="{{ $link }}" class="{{ $sizes[$size]['link'] }} font-medium text-gray-700 dark:text-white">
+      Ver todos →
+    </a>
   @endif
 </div>
