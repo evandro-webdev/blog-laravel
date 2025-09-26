ee727ba6d3debffbb1b3789c7d34da04367507bf
evandro-webdev <evandromateus06@gmail.com>
2025-09-26 17:28:25 -0300

    update: add and update some icons

--- resources/views/components/ui/icons/calendar.blade.php
+++ resources/views/components/ui/icons/calendar.blade.php
@@ -1,3 +1,3 @@
 <x-ui.icon {{ $attributes }}>
-  <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
+  <path d="M8 2v4"/><path d="M16 2v4"/><rect width="18" height="18" x="3" y="4" rx="2"/><path d="M3 10h18"/><path d="M10 16h4"/>
 </x-ui.icon>
---
+++ resources/views/components/ui/icons/clock.blade.php
@@ -0,0 +1,3 @@
+<x-ui.icon {{ $attributes }}>
+  <path d="M12 6v6l4 2"/><circle cx="12" cy="12" r="10"/>
+</x-ui.icon>
