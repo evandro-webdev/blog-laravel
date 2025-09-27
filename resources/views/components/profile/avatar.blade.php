a67b1e39254d4c685ed7d88dc35958c12893b7ac
evandro-webdev <evandromateus06@gmail.com>
2025-09-27 12:12:31 -0300

    update: refactor avatar component

--- resources/views/components/profile/avatar.blade.php
+++ resources/views/components/profile/avatar.blade.php
@@ -1,16 +1,22 @@
-@props([
-  'user',
-  'size' => 'w-10 h-10'
-])
+@props(['user' => null, 'size' => 'w-10 h-10'])
 
-<div class="{{ $size }} rounded-full shrink-0 font-bold text-white overflow-hidden flex items-center justify-center bg-blue-500">
-  @if(!empty($user->profile_pic))
+@if($user)
+  @php
+    $avatar = $user->profile_pic 
+        ? asset('storage/' . $user->profile_pic)
+        : "https://ui-avatars.com/api/?name=" . urlencode($user->name) . "&background=37474f&color=fff";
+  @endphp
+
+  <div class="{{ $size }} rounded-full overflow-hidden bg-gray-300 flex items-center justify-center">
     <img 
-      src="{{ asset('storage/' . $user->profile_pic) }}" 
-      alt="Foto de perfil de {{ $user->name }}" 
+      src="{{ $avatar }}" 
+      alt="{{ $user->name }}"
       class="w-full h-full object-cover"
     >
-  @else
-    {{ strtoupper(substr($user->name, 0, 1)) }}
-  @endif
-</div>
+
+    <div class="w-full h-full flex items-center justify-center text-white font-bold text-sm uppercase bg-gradient-to-br from-blue-500 to-purple-600" 
+        style="display: none;">
+        {{ substr($user->name, 0, 2) }}
+    </div>
+  </div>
+@endif
