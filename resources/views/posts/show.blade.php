6124e7f084adb77768c78047312c02c1e1b13bfc
evandro-webdev <evandromateus06@gmail.com>
2025-10-02 13:52:53 -0300

    feat: add side actions button on show post view

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -14,6 +14,62 @@
   >
   </div>
 
+  <div 
+    class="fixed left-4 top-1/2 -translate-y-1/2 z-40 hidden lg:flex flex-col gap-3"
+    x-data="{ showActions: false }"
+    x-init="
+      window.addEventListener('scroll', () => {
+        showActions = window.pageYOffset > 400;
+      })
+    "
+    x-show="showActions"
+    x-transition
+  >
+    <button 
+      @click="window.scrollTo({ top: 0, behavior: 'smooth' })"
+      class="p-3 rounded-full bg-white dark:bg-gray-700 shadow-lg hover:bg-gray-50 dark:hover:bg-gray-700 transition-colors"
+    >
+      <x-ui.icons.arrow-up class="text-gray-600 dark:text-white"/>
+    </button>
+
+    <div class="relative" x-data="{ open: false }">
+      <button 
+        @click="open = !open"
+        class="p-3 rounded-full bg-white dark:bg-gray-700 shadow-lg hover:bg-gray-50 dark:hover:bg-gray-700 transition-colors"
+      >
+        <x-ui.icons.font-size class="text-gray-600 dark:text-white"/>
+      </button>
+        
+      <div 
+        x-show="open" @click.away="open = false" 
+        x-transition
+        class="ml-2 rounded-lg text-gray-600 dark:text-white absolute left-full top-0 bg-white dark:bg-gray-700 shadow-lg p-2 whitespace-nowrap"
+      >
+        <div class="space-y-1">
+          <button 
+            @click="document.documentElement.style.fontSize = '14px'; open = false"
+            class="block px-3 py-1 rounded text-sm hover:bg-gray-100 dark:hover:bg-gray-800"
+          >
+            Pequeno
+          </button>
+          <button 
+            @click="document.documentElement.style.fontSize = '16px'; open = false"
+            class="block px-3 py-1 rounded text-sm hover:bg-gray-100 dark:hover:bg-gray-800"
+          >
+            Normal
+          </button>
+          <button 
+            @click="document.documentElement.style.fontSize = '18px'; 
+            open = false"
+            class="block px-3 py-1 rounded text-sm hover:bg-gray-100 dark:hover:bg-gray-800"
+          >
+            Grande
+          </button>       
+        </div>
+      </div>
+    </div>
+  </div>
+
   <main class="min-h-screen">
     <header class="relative bg-white dark:bg-gray-800 border-b border-gray-200 dark:border-gray-700">
       <div class="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 pt-12 lg:pt-16">
