135a861c90ddb633554d50d0d4b6f2396a2741ef
evandro-webdev <evandromateus06@gmail.com>
2025-10-03 13:18:39 -0300

    feat: delete unused cards

--- resources/views/components/blog/news/news-card.blade.php
+++
@@ -1,9 +0,0 @@
-<x-ui.panel>
-  <h3 class="mb-2 text-xl font-bold line-clamp-2 text-gray-800">Complete Web Development Roadmap 2024</h3>
-  <div class="text-sm font-medium text-gray-600 flex gap-x-2 items-center">
-    <x-ui.icons.calendar class="text-blue-600"/>
-    <span>12 de Maio, 2025</span>
-    <span>·</span>
-    <span>Business Tech</span>
-  </div>
-</x-ui.panel>
