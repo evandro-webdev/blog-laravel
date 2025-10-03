8b0ebfc3984ef2603ada5f0d8d93c3198052b28b
evandro-webdev <evandromateus06@gmail.com>
2025-10-03 13:24:39 -0300

    update: change home tabs to use cards components

--- resources/views/components/home/tabs/tab-trending-feed.blade.php
+++ resources/views/components/home/tabs/tab-trending-feed.blade.php
@@ -15,44 +15,7 @@
 
   <div class="space-y-4">
     @foreach ($posts as $post)
-      <div class="rounded-md overflow-hidden flex flex-col sm:flex-row ">
-        <x-blog.post.post-thumbnail :link="$post->image" class="sm:w-1/3 sm:aspect-[4/3] object-cover"/>
-        
-        <div class="p-4 rounded-b-md sm:rounded-l-none sm:rounded-t-md 
-          border-1 border-t-0 sm:border-t-1 sm:border-l-0 
-          border-gray-200 dark:border-gray-700 space-y-4 bg-white dark:bg-gray-900 flex flex-col flex-1 justify-center"
-        >
-          <div class="flex items-center gap-2">
-            <div class="flex items-center gap-2">
-              <x-profile.avatar :user="$post->user" size="w-7 h-7"/>
-              <span class="text-sm font-medium text-gray-700 dark:text-gray-100">{{ $post->user->name }}</span>
-            </div>
-            <span class="text-gray-600 dark:text-gray-100">·</span>
-            <time class="text-xs text-gray-600 dark:text-gray-100" datetime="{{ $post->created_at }}">
-              {{ $post->created_at->diffForHumans() }}
-            </time>
-          </div>
-
-          <a href="{{ route('posts.show', $post) }}" class="space-y-1">
-            <h3 class="font-bold text-xl text-gray-800 dark:text-white">{{ $post->title }}</h3>
-            <p class="text-gray-600 dark:text-gray-100 line-clamp-2">{{ $post->excerpt }}</p>
-          </a>
-
-          <div class="flex items-center justify-between">
-            <x-ui.badge href="#" small>{{ $post->category->name }}</x-ui.badge>
-            <div class="text-gray-600 dark:text-gray-100 flex items-center gap-2">
-              <div class="flex items-center gap-1">
-                <x-ui.icons.save size="w-4 h-4"/>
-                <span class="text-xs">{{ $post->savedBy->count() }}</span>
-              </div>
-              <div class="flex items-center gap-1">
-                <x-ui.icons.comment size="w-4 h-4"/>
-                <span class="text-xs">{{ $post->comments->count() }}</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <x-blog.post.post-card-horizontal :$post/>
     @endforeach
   </div>
 </div>
