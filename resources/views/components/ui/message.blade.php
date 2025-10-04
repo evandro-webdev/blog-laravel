28612d9e956c8112b499a3f16030abcb0d64e1d2
evandro-webdev <evandromateus06@gmail.com>
2025-10-04 12:02:31 -0300

    feat: create message component and add more icons

---
+++ resources/views/components/ui/message.blade.php
@@ -0,0 +1,28 @@
+@props([
+  'message',
+  'type' => 'default',
+  'icon' => 'info'
+])
+
+@php
+  $variantStyles = [
+    'default' => 'border border-gray-300 dark:border-0 text-gray-600 dark:text-gray-300 bg-gray-50 dark:bg-gray-700',
+    'success' => 'text-green-600 dark:text-green-300 bg-green-50 dark:bg-green-950',
+    'alert' => 'border border-yellow-200 dark:border-0 text-yellow-600 dark:text-yellow-300 bg-yellow-50 dark:bg-yellow-800',
+    'danger' => 'text-red-600 dark:text-red-300 bg-red-100 dark:bg-red-950',
+  ];
+
+  $icons = [
+    'default' => $icon,
+    'success' => 'check',
+    'alert' => 'alert',
+    'danger' => 'danger',
+  ];
+
+  $classes = 'p-4 rounded-md ' . $variantStyles[$type];
+@endphp
+
+<div class="p-4 rounded-md {{ $classes }} flex items-center gap-2">
+  <x-dynamic-component :component="'ui.icons.' . $icons[$type]"/>
+  <p>{{ $message }}</p>
+</div>
