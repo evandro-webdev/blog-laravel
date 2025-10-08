dc623fec88e5c46ac3024ef167eb18526106a28b
evandro-webdev <evandromateus06@gmail.com>
2025-10-08 13:23:56 -0300

    update: fix use of panel component on auth-prompt component

--- resources/views/components/ui/utilities/auth-prompt.blade.php
+++ resources/views/components/ui/utilities/auth-prompt.blade.php
@@ -2,11 +2,11 @@
   'message' => 'Faça login para continuar'
 ])
 
-<x-ui.panel class="text-center flex flex-col items-center gap-4">
+<x-ui.base.panel class="text-center flex flex-col items-center gap-4">
   <p class="text-gray-600 dark:text-white">{{ $message }}</p>
   <x-ui.forms.button href="{{ route('login') }}" icon="enter" size="sm">Entrar</x-ui.forms.button>
   <p class="text-sm text-gray-500 dark:text-gray-100">
     Ainda não tem conta?
     <a href="{{ route('register') }}" class="text-blue-600 dark:text-blue-400 hover:underline">Cadastre-se</a>
   </p>
-</x-ui.panel>
+</x-ui.base.panel>
