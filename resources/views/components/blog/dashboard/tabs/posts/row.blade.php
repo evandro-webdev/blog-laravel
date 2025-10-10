fb0b5ac28b5d3862b59b5131e0ce7b998c21814d
evandro-webdev <evandromateus06@gmail.com>
2025-10-10 13:31:44 -0300

    update: add confirmation modal to delete post on post row component

--- resources/views/components/blog/dashboard/tabs/posts/row.blade.php
+++ resources/views/components/blog/dashboard/tabs/posts/row.blade.php
@@ -1,6 +1,8 @@
 <tr class="odd:bg-gray-50 dark:odd:bg-slate-700">
   <td class="max-w-[50ch] px-3 py-5 truncate text-gray-800 dark:text-white" title="{{ $post->title }}">
-    {{ $post->title }}
+    <a href="/posts/{{ $post->slug }}" target="_blank">
+      {{ $post->title }}
+    </a>
   </td>
   <th class="px-3 py-3 hidden font-medium text-left text-gray-600 dark:text-gray-100 md:table-cell">{{ $post->category->name }}</th>
   <td class="px-3 py-5 hidden sm:table-cell text-gray-600 dark:text-gray-100 whitespace-nowrap">
@@ -20,18 +22,26 @@
   </td>
 
   <td class="px-3 py-5 text-right whitespace-nowrap">
-    <div class="flex justify-end gap-4 text-gray-500 dark:text-white">
-      <a href="/posts/{{ $post->slug }}" target="_blank">
-        <x-ui.icons.eye/>
-      </a>
+    <div class="flex items-center justify-end gap-4 text-gray-500 dark:text-white">
       <a href="/admin/posts/{{ $post->id }}/edit">
-        <x-ui.icons.edit/>
+        <x-ui.icons.edit class="text-blue-600 dark:text-blue-500"/>
       </a>
-      <form action="/admin/posts/{{ $post->id }}" method="POST">
+
+      <form 
+        action="{{ route('posts.destroy', $post->id) }}" 
+        method="POST" class="flex"
+      >
         @csrf
         @method('DELETE')
-        <button class="cursor-pointer">
-          <x-ui.icons.trash/>
+
+        <button 
+          type="button"
+          @click.prevent="$dispatch('open-modal', {
+            onConfirm: () => $el.closest('form').submit()
+          })" 
+          class="cursor-pointer"
+        >
+          <x-ui.icons.trash class="text-red-600 dark:text-red-500"/>
         </button>
       </form>
     </div>
