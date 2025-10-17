37fe63c77eee30ced83f457d718e4533b0aca538
evandro-webdev <evandromateus06@gmail.com>
2025-10-17 17:19:15 -0300

    update: remove is_admin from users table and add role field

--- routes/web.php
+++ routes/web.php
@@ -51,7 +51,7 @@ Route::middleware('auth')->group(function () {
   Route::post('/notifications/read', [NotificationController::class, 'markAllAsRead']);
 });
 
-Route::middleware(['admin', 'auth'])->prefix('/admin')->group(function () {
+Route::middleware(['auth'])->prefix('/admin')->group(function () {
   Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('admin.dashboard');
   Route::resource('posts', PostController::class)->except(['index', 'show']);
 });
