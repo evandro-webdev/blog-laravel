75084cbc24447c827885654f4f952dcf8277d9bb
evandro-webdev <evandromateus06@gmail.com>
2025-10-17 13:36:06 -0300

    update: checkbox ui

--- resources/views/components/ui/forms/checkbox.blade.php
+++ resources/views/components/ui/forms/checkbox.blade.php
@@ -1,7 +1,6 @@
 @props([
   'label',
   'name',
-  'type' => 'checkbox',
   'id' => $name,
   'checked' => false,
   'tip' => null
@@ -12,11 +11,29 @@
   :label="false"
   :$tip
 >
-  <input
-    type="{{ $type }}"
-    id="{{ $id }}"
-    name="{{ $name }}"
-    @checked(old($name, $checked))
+  <div
+    x-data="{ checked: {{ old($name, $checked) ? 'true' : 'false' }} }" 
+    class="flex items-center gap-2"
   >
-  <x-ui.forms.label :$label :$name class="mb-0! ml-1"/>
+    <input
+      type="checkbox"
+      hidden
+      id="{{ $id }}"
+      name="{{ $name }}"
+      x-model="checked"
+      {{ $attributes->merge(['class']) }}
+    >
+
+    <label for="{{ $id }}" class="cursor-pointer text-gray-600 dark:text-gray-300">
+      <template x-if="!checked">
+        <x-ui.icons.square/> 
+      </template>
+  
+      <template x-if="checked">
+        <x-ui.icons.checked-square/> 
+      </template>
+    </label>
+
+    <x-ui.forms.label :$label :$id class="mb-0!"/>
+  </div>
 </x-ui.forms.field>
