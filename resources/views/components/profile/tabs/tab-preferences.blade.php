ee375644dd139f2b0121a1336f0a40202f49d399
evandro-webdev <evandromateus06@gmail.com>
2025-10-21 13:46:55 -0300

    update: create delete account section on preferences-tab

--- resources/views/components/profile/tabs/tab-preferences.blade.php
+++ resources/views/components/profile/tabs/tab-preferences.blade.php
@@ -44,56 +44,99 @@
         size="sm"
         icon="padlock"
       >
-        <span x-text="show ? 'Cancelar' : 'Alterar senha'"></span>
+        <span x-text="show ? 'Ocultar' : 'Mostrar'"></span>
       </x-ui.forms.button>
     </div>
 
-    <form 
+    <div 
       x-show="show"
       x-collapse.duration.1000ms
-      @submit.prevent="submit"
-      action="{{ route('settings.updatePassword') }}" 
-      method="POST" 
       class="mt-6 space-y-6"
     >
-      @csrf
-      @method('PATCH')
-      <div class="space-y-4">
-        <x-ui.forms.input 
-          x-model="fields.current" 
-          type="password" 
-          name="current_password" 
-          x-error="errors.current"
-          placeholder="Digite sua senha atual"
-        />
-
-        <x-ui.forms.input 
-          x-model="fields.new" 
-          type="password" name="password" 
-          x-error="errors.new" 
-          placeholder="Digite sua nova senha"
-        />
-
-        <x-ui.forms.input 
-          x-model="fields.confirm" 
-          type="password" 
-          name="password_confirmation" 
-          x-error="errors.confirm" 
-          placeholder="Confirme sua nova senha"
-        />
-      </div>
-
-      <x-ui.forms.button 
-        type="button" 
-        @click="submit($event)"
+      <x-section-heading
+        title="Alterar senha"
+        desc="Confira ou edite suas preferências"
+        class="mb-4"
         size="sm"
-      >
-        <span x-show="!loading">Salvar</span>
-        <x-ui.base.spinner x-show="loading"/>
-      </x-ui.forms.button>
-    </form>
+      />
 
-    <x-ui.toast position="center-top" icon="key"/>
+      <form 
+        @submit.prevent="submit"
+        action="{{ route('settings.updatePassword') }}" 
+        method="POST" 
+        class="space-y-4"
+      >
+        @csrf
+        @method('PATCH')
+        <div class="space-y-2">
+          <x-ui.forms.input 
+            x-model="fields.current" 
+            type="password" 
+            name="current_password" 
+            x-error="errors.current"
+            placeholder="Digite sua senha atual"
+          />
+
+          <x-ui.forms.input 
+            x-model="fields.new" 
+            type="password" name="password" 
+            x-error="errors.new" 
+            placeholder="Digite sua nova senha"
+          />
+
+          <x-ui.forms.input 
+            x-model="fields.confirm" 
+            type="password" 
+            name="password_confirmation" 
+            x-error="errors.confirm" 
+            placeholder="Confirme sua nova senha"
+          />
+        </div>
+
+        <x-ui.forms.button 
+          type="button" 
+          @click="submit($event)"
+          size="sm"
+        >
+          <span x-show="!loading">Salvar</span>
+          <x-ui.base.spinner x-show="loading"/>
+        </x-ui.forms.button>
+      </form>
+
+      <hr class="text-gray-300 dark:text-slate-700">
+
+      <div class="flex justify-between items-center">
+        <x-section-heading
+          title="Excluir conta"
+          desc="Apagar conta e todos seus dados"
+          class="mb-4"
+          size="sm"
+        />
+
+        <form 
+          action="{{ route('settings.deleteAccount') }}" 
+          method="POST"
+          class="flex gap-2"
+        >
+          @csrf
+          @method('DELETE')
+
+          <x-ui.forms.input name="passwordDelete" type="password" size="sm" required placeholder="Confirme sua senha"/>
+          <x-ui.forms.button
+            @click.prevent="$dispatch('open-modal', {
+              title: 'Excluir conta?',
+              content: 'Ao excluir sua conta, você perderá todos os seus posts. Tem certeza que deseja fazer isso?',
+              onConfirm: () => $el.closest('form').submit()
+            })"
+            type="button"
+            size="sm" 
+            variant="danger"
+          >
+            Excluir conta
+          </x-ui.forms.button>
+        </form>
+      </div>
+    </div>
   </x-ui.base.panel>
 </div>
 
