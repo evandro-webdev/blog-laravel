24030db4698c7ff773b0c05481d5f713544b4870
evandro-webdev <evandromateus06@gmail.com>
2025-10-22 17:12:14 -0300

    update: implement authorization policies on posts table from dashboard

--- resources/views/components/blog/dashboard/tabs/posts/row.blade.php
+++ resources/views/components/blog/dashboard/tabs/posts/row.blade.php
@@ -1,17 +1,19 @@
 <tr class="odd:bg-gray-50 dark:odd:bg-slate-900">
-  <td class="max-w-[50ch] px-5 py-5 truncate text-gray-800 dark:text-white" title="{{ $post->title }}">
+  <td class="max-w-[50ch] p-5 truncate text-gray-800 dark:text-white" title="{{ $post->title }}">
     <a href="/posts/{{ $post->slug }}" target="_blank">
       {{ $post->title }}
     </a>
   </td>
-  <th class="px-5 py-3 hidden font-medium text-left text-gray-600 dark:text-gray-100 md:table-cell">{{ $post->category->name }}</th>
-  <td class="px-5 py-5 hidden sm:table-cell text-gray-600 dark:text-gray-100 whitespace-nowrap">
+  <td class="p-5 hidden font-medium text-left text-gray-600 dark:text-gray-100 md:table-cell">
+    {{ $post->category->name }}
+  </td>
+  <td class="p-5 hidden sm:table-cell text-gray-600 dark:text-gray-100 whitespace-nowrap">
     {{ $post->created_at->format('d/m/Y') }}
   </td>
-  <td class="px-5 py-5 hidden lg:table-cell text-gray-600 dark:text-gray-100 whitespace-nowrap">
+  <td class="p-5 hidden lg:table-cell text-gray-600 dark:text-gray-100 whitespace-nowrap">
     {{ $post->views->count() }}
   </td>
-  <td class="px-5 py-5 whitespace-nowrap">
+  <td class="p-5 whitespace-nowrap">
     @if ($post->featured)
       <x-ui.base.badge variant="blue" small>Destaque</x-ui.base.badge>
     @elseif ($post->published)
@@ -21,32 +23,36 @@
     @endif
   </td>
 
-  <td class="px-5 py-5 text-right whitespace-nowrap">
+  <td class="p-5 text-right whitespace-nowrap">
     <div class="flex items-center justify-end gap-4 text-gray-500 dark:text-white">
-      <a href="/admin/posts/{{ $post->id }}/edit">
-        <x-ui.icons.edit size="w-6 h-6" class="text-blue-600 dark:text-blue-500"/>
-      </a>
+      @can('update', $post)
+        <a href="{{ route('posts.edit', $post) }}">
+          <x-ui.icons.edit size="w-6 h-6" class="text-blue-600 dark:text-blue-500"/>
+        </a>
+      @endcan
 
-      <form 
-        action="{{ route('posts.destroy', $post->id) }}" 
-        method="POST" 
-        class="flex"
-      >
-        @csrf
-        @method('DELETE')
-
-        <button 
-          type="button"
-          @click.prevent="$dispatch('open-modal', {
-            title: 'Tem certeza?',
-            content: 'Excluir um post é uma ação irreversivel, confirme ou cancele abaixo',
-            onConfirm: () => $el.closest('form').submit()
-          })" 
-          class="cursor-pointer"
+      @can('delete', $post)  
+        <form 
+          action="{{ route('posts.destroy', $post) }}" 
+          method="POST" 
+          class="flex"
         >
-          <x-ui.icons.trash size="w-6 h-6" class="text-red-600 dark:text-red-500"/>
-        </button>
-      </form>
+          @csrf
+          @method('DELETE')
+
+          <button 
+            type="button"
+            @click.prevent="$dispatch('open-modal', {
+              title: 'Tem certeza?',
+              content: 'Excluir um post é uma ação irreversivel, confirme ou cancele abaixo',
+              onConfirm: () => $el.closest('form').submit()
+            })" 
+            class="cursor-pointer"
+          >
+            <x-ui.icons.trash size="w-6 h-6" class="text-red-600 dark:text-red-500"/>
+          </button>
+        </form>
+      @endcan
     </div>
   </td>
 </tr>
