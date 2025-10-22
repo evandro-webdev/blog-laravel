2fae07c0e7e4af3447d9e8d2e8058bc737efe0a1
evandro-webdev <evandromateus06@gmail.com>
2025-10-22 17:21:29 -0300

    update: add prop to determine if follow button is shown or not

--- resources/views/components/home/users-list.blade.php
+++ resources/views/components/home/users-list.blade.php
@@ -1,22 +1,25 @@
 @props([
-  'usersToFollow'
+  'users',
+  'follow' => false
 ])
 
 <div class="space-y-1.5">
-  @foreach ($usersToFollow as $userToFollow)
+  @foreach ($users as $user)
     <div class="flex items-center justify-between p-2 rounded-md hover:bg-gray-100 dark:hover:bg-slate-700 transition-colors group">
-      <a href="{{ route('profile.show', $userToFollow) }}" class="flex items-center gap-2">
-        <x-profile.avatar :user="$userToFollow" size="w-10 h-10"/>
+      <a href="{{ route('profile.show', $user) }}" class="flex items-center gap-2">
+        <x-profile.avatar :user="$user" size="w-10 h-10"/>
         <div>
           <span class="block text-sm font-medium text-gray-800 dark:text-white">
-            {{ $userToFollow->name }}
+            {{ $user->name }}
           </span>
           <span class="block text-xs text-gray-500 dark:text-gray-400">
-            {{ $userToFollow->posts->count() }} posts
+            {{ $user->posts->count() }} posts
           </span>
         </div>
       </a>
-      <x-ui.interactive.follow-button :user="$userToFollow"/>
+      @if ($follow)
+        <x-ui.interactive.follow-button :user="$user"/>
+      @endif
     </div>
   @endforeach
 </div>
