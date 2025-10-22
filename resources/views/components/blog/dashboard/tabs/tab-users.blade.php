00a5baf1034d976ae52a2c3805d55a03a95e4739
evandro-webdev <evandromateus06@gmail.com>
2025-10-22 17:20:43 -0300

    update implement delete user and update role on tab-users from dashboard

--- resources/views/components/blog/dashboard/tabs/tab-users.blade.php
+++ resources/views/components/blog/dashboard/tabs/tab-users.blade.php
@@ -14,38 +14,51 @@
       <table class="w-full text-sm table-auto">
         <thead class="border-b border-gray-200 dark:border-gray-700">
           <tr class="text-left text-gray-600 dark:text-white">
-            <th class="px-3 py-5">Nome</th>
-            <th class="px-3 py-5 hidden md:table-cell">Posts</th>
-            <th class="px-3 py-5 hidden sm:table-cell">Membro desde</th>
-            <th class="px-3 py-5 hidden lg:table-cell">Seguidores</th>
-            <th class="px-3 py-5 whitespace-nowrap">Seguindo</th>
-            <th class="min-w-[140px] px-3 py-5 text-right whitespace-nowrap">Ações</th>
+            <th class="p-5">Nome</th>
+            <th class="p-5 hidden md:table-cell">Posts</th>
+            <th class="p-5 hidden sm:table-cell">Membro desde</th>
+            <th class="p-5 hidden lg:table-cell">Seguidores</th>
+            <th class="p-5 hidden lg:table-cell">Seguindo</th>
+            <th class="p-5 whitespace-nowrap">Cargo</th>
+            <th class="min-w-[140px] p-5 text-right whitespace-nowrap">Ações</th>
           </tr>
         </thead>
         <tbody>
           @foreach ($users as $user)
             <tr class="odd:bg-gray-50 dark:odd:bg-slate-900">
-              <td class="max-w-[50ch] px-5 py-5 truncate text-gray-800 dark:text-white">
+              <td class="p-5 text-gray-800 dark:text-white">
                 <a href="{{ route('profile.show', $user) }}" target="_blank">
                   {{ $user->name }}
                 </a>
               </td>
-              <td class="px-5 py-5 hidden lg:table-cell text-gray-600 dark:text-gray-100 whitespace-nowrap">
+              <td class="p-5 hidden lg:table-cell text-gray-600 dark:text-gray-100 whitespace-nowrap">
                 {{ $user->posts->count() }}
               </td>
-              <td class="px-5 py-5 hidden sm:table-cell text-gray-600 dark:text-gray-100 whitespace-nowrap">
+              <td class="p-5 hidden sm:table-cell text-gray-600 dark:text-gray-100 whitespace-nowrap">
                 {{ $user->created_at->format('d/m/Y') }}
               </td>
-              <td class="px-5 py-5 hidden lg:table-cell text-gray-600 dark:text-gray-100 whitespace-nowrap">
+              <td class="p-5 hidden lg:table-cell text-gray-600 dark:text-gray-100 whitespace-nowrap">
                 {{ $user->getFollowersCount() }}
               </td>
-              <td class="px-5 py-5 hidden lg:table-cell text-gray-600 dark:text-gray-100 whitespace-nowrap">
+              <td class="p-5 hidden lg:table-cell text-gray-600 dark:text-gray-100 whitespace-nowrap">
                 {{ $user->getFollowingCount() }}
               </td>
-              <td class="px-5 py-5 text-right whitespace-nowrap">
+              <td class="p-5 hidden lg:table-cell text-gray-600 dark:text-gray-100 whitespace-nowrap">
+                <form action="{{ route('admin.users.updateRole', $user) }}" method="POST">
+                  @csrf
+                  @method('PATCH')
+
+                  <select name="role" onchange="this.form.submit()">
+                    <option value="author" {{ $user->role === 'author' ? 'selected' : '' }}>Autor</option>
+                    <option value="moderator" {{ $user->role === 'moderator' ? 'selected' : '' }}>Moderador</option>
+                    <option value="admin" {{ $user->role === 'admin' ? 'selected' : '' }}>Administrador</option>
+                  </select>
+                </form>
+              </td>
+              <td class="p-5 text-right whitespace-nowrap">
                 <div class="flex items-center justify-end gap-4 text-gray-500 dark:text-white">
                   <form 
-                    {{-- action="{{ route('posts.destroy', $post->id) }}"  --}}
+                    action="{{ route('admin.users.destroy', $user) }}" 
                     method="POST" 
                     class="flex"
                   >
@@ -54,13 +67,13 @@
 
                     <x-ui.forms.button 
                       @click.prevent="$dispatch('open-modal', {
-                        title: 'Tem certeza?',
-                        content: 'Excluir um post é uma ação irreversivel, confirme ou cancele abaixo',
+                        title: 'Excluir usuário?',
+                        content: 'Tem certeza que deseja excluir esse usuário?',
                         onConfirm: () => $el.closest('form').submit()
                       })"
                       type="button"
                       icon="trash"
-                      size="sm"
+                      size="xs"
                       variant="danger"
                     >
                       Excluir
